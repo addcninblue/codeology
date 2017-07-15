c745e4458c31c5036e24ac9f069d208bcffb4a0a
hengloose12 <FranklinHeng@Franklins-MacBook-Air.local>
2017-07-14 21:27:13 -0700

    Simple d3 graph test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,7 +14,7 @@ class StaticPagesController < ApplicationController
   def team
     render 'team'
   end
-  
+
   def recruitment
     render 'recruitment'
   end
@@ -25,6 +25,24 @@ class StaticPagesController < ApplicationController
 
   def edward
     render 'edward'
+  end
+
+  def test
+
+    data = {
+      "nodes": [
+        {"id": "Albert", "group": 1},
+        {"id": "Edward", "group": 2},
+        {"id": "Franklin", "group": 3}
+      ],
+      "links": [
+        {"source": "Albert", "target": "Edward", "value": 3},
+        {"source": "Edward", "target": "Franklin", "value": 50},
+        {"source": "Franklin", "target": "Albert", "value": 1}
+      ]
+    }
+
+    render :json => data
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+  get '/test', to: "static_pages#test"
+
   resources :users
 end
