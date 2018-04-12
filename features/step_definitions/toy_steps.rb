76b0cbf04f287a77031df919df4f532aace02a1e
Marcus Lee <marcuslee143@berkeley.edu>
2018-04-12 13:24:14 -0700

    step definitions implemented and page route reimplemented. this branch will be scrapped in the near future

--- features/step_definitions/toy_steps.rb
+++ features/step_definitions/toy_steps.rb
@@ -1,11 +1,15 @@
-# Given /^(?:|I )am on the (.+) page$/ do |page_name|
-#   pending
-# end
+Given /^(?:|I )am on the (.+) page$/ do |page_name|
+  if page_name == "home"
+  	visit('/')
+  else
+  	visit('/' + page_name)
+  end
+end
 
-# When("I follow {string}") do |string|
-#   pending
-# end
+When "I follow {string}" do |string|
+  click_link(string)
+end
 
-# Then /^(?:|I )should be on the (.+) page$/ do |page_name|
-#   pending
-# end
+Then /^(?:|I )should be on the (.+) page$/ do |page_name|
+  expect(page).to have_current_path('/' + page_name)
+end
